301cdce8f6808f0c8aaca269fedfa008c0eeadef
DESKTOP-M451O6I\John <f40706@gmail.com>
2022-03-19 17:28:59 +0800

    新增CoroutineScope Exception處理

--- app/src/main/java/com/john/coroutine_scope_exception/MainActivity.kt
+++ app/src/main/java/com/john/coroutine_scope_exception/MainActivity.kt
@@ -2,10 +2,45 @@ package com.john.coroutine_scope_exception
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.john.coroutine_scope_exception.databinding.ActivityMainBinding
+import com.john.coroutine_scope_exception.model.MainBrain
+import com.john.coroutine_scope_exception.view.MainView
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
+    //View
+    private lateinit var mMainView: MainView
+    //Model
+    private val mainBrain = MainBrain()
+    //ViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initView()
+        setContentView(mMainView.getRootView())
+        setupView()
+        initEvent()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mainBrain.release()
+    }
+
+    private fun initView() {
+        mMainView = MainView(this)
+    }
+
+    private fun setupView() {
+        mMainView.initView()
+    }
+
+    private fun initEvent() {
+        mMainView.initEvent {
+            mainBrain.processCallbackEvent(it)
+        }
     }
 }
---
+++ app/src/main/java/com/john/coroutine_scope_exception/type/BtnTagEnum.kt
@@ -0,0 +1,5 @@
+package com.john.coroutine_scope_exception.type
+
+enum class BtnTagEnum {
+    Btn1
+}
